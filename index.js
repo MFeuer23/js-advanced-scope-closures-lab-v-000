de56f38288cf65ad8db74424c18ffab55dbd2a0d
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-27 15:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function produceDrivingRange(blockRange) {
-  return function range (a, b) {
-    let num = Math.abs(parseInt(a) - parseInt(b))
-    if (num > blockRange) {
-      return `${Math.abs(num - blockRange)} blocks out of range`
+  return function (a, b) {
+    let range = Math.abs(parseInt(a) - parseInt(b))
+    if (range > blockRange) {
+      return `${Math.abs(range - blockRange)} blocks out of range`
     } else {
-      return `within range by ${Math.abs(num - blockRange)}`
+      return `within range by ${Math.abs(range - blockRange)}`
     }
   }
 }
