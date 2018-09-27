029d63bfef1d9a67c1c955e08ac8e4a7f9b361ac
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-27 15:33:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function produceDrivingRange(blockRange) {
+  return function (a, b) {
+    let range = Math.abs(parseInt(a) - parseInt(b))
+    if (range > blockRange) {
+      return `${Math.abs(range - blockRange)} blocks out of range`
+    } else {
+      return `within range by ${Math.abs(range - blockRange)}`
+    }
+  }
+}
+
+function produceTipCalculator(percentage) {
+  return function tip(fare) {
+    return fare * percentage;
+  }
+}
+
+function createDriver() {
+  let driverId = 0
+  return class Driver {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
