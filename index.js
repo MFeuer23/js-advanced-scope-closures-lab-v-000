d86b0690949de9dfd8fb2f7e6eb0bf6232539a6c
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-27 15:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function produceDrivingRange(blockRange) {
   return function range (a , b) {
     if (Math.abs(a-b) > blockRange) {
-      return ""
+      return `${Math.abs(a-b)} blocks out of range`
+    } else {
+      return `within range by ${Math.abs(a-b)}`
     }
   }
 }
